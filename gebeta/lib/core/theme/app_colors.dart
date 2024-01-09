83e6b4beca819f39c26d54f3fb8784403e1ef283
betabravah <betabravah@gmail.com>
2024-01-09 16:32:03 +0300

    Add App Theme

---
+++ gebeta/lib/core/theme/app_colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+
+  static const pink = Color(0xFFFB2B74);
+  static const translucentBlue = Color(0x26376AED);
+  static const blue100 = Color(0xFFBBDEFB);
+  static const saturatedPink200 = Color(0xFFED387A);
+  static const saturatedPink300 = Color(0xFFCD216E);
+  static const darkBlue = Color(0xFF792D4B);
+  static const darkerPink = Color(0xFF3C0D23);
+  static const lightPink = Color(0xFFFF6696);
+
+  static const darkGray = Color(0xFF7B8BB2);
+
+  static const gray100 = Color(0xFFF8FAFF);
+  static const gray200 = Color(0xFFD7DDEB);
+  static const gray300 = Color(0xFF979EA5);
+  static const gray400 = Color(0xFF878585);
+  static const gray500 = Color(0x427B8BB2);
+  static const gray600 = Color(0xFF5E5F6F);
+  static const gray700 = Color(0xFF4D4A49);
+  static const grayDark = Color(0xFF424242);
+  static const grayLight = Color(0xFFC6D6DF);
+
+  static const red = Color(0xFFCC3926);
+}
