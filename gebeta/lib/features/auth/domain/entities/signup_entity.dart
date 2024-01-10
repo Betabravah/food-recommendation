9160339cae597737ca4d17632364eafc2cdd7d24
betabravah <betabravah@gmail.com>
2024-01-10 14:24:43 +0300

    Add Auth Entities

---
+++ gebeta/lib/features/auth/domain/entities/signup_entity.dart
@@ -0,0 +1,52 @@
+import 'package:equatable/equatable.dart';
+
+class SignupEntity extends Equatable {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String password;
+  final double height;
+  final double weight;
+  final double upperLegLength;
+  final double upperArmLength;
+  final double armCircumference;
+  final double waistCircumference;
+  final double hipCircumference;
+  final double systolic;
+  final double diastolic;
+  final double pulse;
+
+  const SignupEntity(
+      {required this.firstName,
+      required this.lastName,
+      required this.email,
+      required this.password,
+      required this.height,
+      required this.weight,
+      required this.upperLegLength,
+      required this.upperArmLength,
+      required this.armCircumference,
+      required this.waistCircumference,
+      required this.hipCircumference,
+      required this.systolic,
+      required this.diastolic,
+      required this.pulse});
+
+  @override
+  List<Object?> get props => [
+        firstName,
+        lastName,
+        email,
+        password,
+        height,
+        weight,
+        upperLegLength,
+        upperArmLength,
+        armCircumference,
+        waistCircumference,
+        hipCircumference,
+        systolic,
+        diastolic,
+        pulse
+      ];
+}
