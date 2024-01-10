4ef616b4824bff48f3514221273e582ce0ae02ba
betabravah <betabravah@gmail.com>
2024-01-10 22:49:35 +0300

    Add Const COnstructor

--- gebeta/lib/features/auth/domain/entities/login_entity.dart
+++ gebeta/lib/features/auth/domain/entities/login_entity.dart
@@ -4,7 +4,7 @@ class LoginEntity extends Equatable {
   final String email;
   final String password;
 
-  const LoginEntity(this.email, this.password);
+  const LoginEntity({required this.email, required this.password});
 
   @override
   List<Object?> get props => [email, password];
