9f05566a14820e73df3c6ac13d06c10e0b3fd030
betabravah <betabravah@gmail.com>
2024-01-11 09:40:47 +0300

    Add Signup Model Mapper

---
+++ gebeta/lib/features/auth/data/models/signup_mapper.dart
@@ -0,0 +1,23 @@
+import '../../domain/entities/signup_entity.dart';
+import 'signup_model.dart';
+
+extension on SignupEntity {
+  SignupModel toModel() {
+    return SignupModel(
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+      height: height,
+      weight: weight,
+      upperLegLength: upperLegLength,
+      upperArmLength: upperArmLength,
+      armCircumference: armCircumference,
+      waistCircumference: waistCircumference,
+      hipCircumference: hipCircumference,
+      systolic: systolic,
+      diastolic: diastolic,
+      pulse: pulse,
+    );
+  }
+}
