a69f9c6a227b6abc19d47234e8459bf5452886b1
betabravah <betabravah@gmail.com>
2024-01-11 15:38:29 +0300

    Fix Food Repository

--- gebeta/lib/features/food/domain/repository/food_repository.dart
+++ gebeta/lib/features/food/domain/repository/food_repository.dart
@@ -1,10 +1,9 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 
 import '../../../../core/error/failure.dart';
 import '../entities/food.dart';
 
-abstract class FoodRepository extends Equatable {
+abstract class FoodRepository {
   Future<Either<Failure, Food>> getFood(String id);
   Future<Either<Failure, List<Food>>> getFoods();
 }
