ca73436e206d5c27facb3e60c3b05c8e7462ad9f
betabravah <betabravah@gmail.com>
2024-01-11 10:40:58 +0300

    Add User Repository Implementation

---
+++ gebeta/lib/features/user/data/repository/user_repository_impl.dart
@@ -0,0 +1,87 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/error/exceptions.dart';
+import '../../../../core/error/failure.dart';
+import '../../../../core/network/network_info.dart';
+import '../../domain/entities/user.dart';
+import '../../domain/repository/user_repository.dart';
+import '../datasources/local/local_datasource.dart';
+import '../datasources/remote/remote_datasource.dart';
+import '../models/user_model.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final RemoteDataSource remoteDataSource;
+  final LocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  UserRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+    required this.networkInfo,
+  });
+
+  @override
+  Future<Either<Failure, User>> getUser(String token) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteUser = await remoteDataSource.getUser(token);
+        localDataSource.cacheUser(remoteUser);
+        return Right(remoteUser);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localUser = await localDataSource.getUser(token);
+        return Right(localUser);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, User>> deleteUser(String token) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteUser = await remoteDataSource.deleteUser(token);
+        localDataSource.cacheUser(remoteUser);
+        return Right(remoteUser);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(NetworkFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, User>> updateUser(User user) async {
+    if (await networkInfo.isConnected) {
+      UserModel userModel = UserModel(
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          password: user.password,
+          height: user.height,
+          weight: user.weight,
+          upperArmLength: user.upperArmLength,
+          armCircumference: user.armCircumference,
+          hipCircumference: user.hipCircumference,
+          waistCircumference: user.waistCircumference,
+          upperLegLength: user.upperLegLength,
+          systolic: user.systolic,
+          diastolic: user.diastolic,
+          pulse: user.pulse);
+      try {
+        final remoteUser = await remoteDataSource.updateUser(userModel);
+        localDataSource.cacheUser(remoteUser);
+        return Right(remoteUser);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(NetworkFailure());
+    }
+  }
+}
